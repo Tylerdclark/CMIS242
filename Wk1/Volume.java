a733ef6b0152e6d3a7533535192e4fc1f08a4a93
Tyler Clark <Tylerdclark@users.noreply.github.com>
2020-01-18 18:09:55 -0500

    Add in wk1 folder

---
+++ Wk1/Volume.java
@@ -0,0 +1,30 @@
+public class Volume{
+
+    public int MIN_VOL = 0;
+    public int MAX_VOL = 100 ; 
+
+    static int Volume ;
+
+    volumeUp() {
+        main.Volume++;
+    }
+
+    VolumeDow(){
+        main.Volume--;
+
+    }
+
+    SetVolume( final int Volume){
+       main.Volume = Volume;
+       if (main.Volume > MAX_VOL){
+           main.Volume = MAX_VOL ; 
+       }
+       if(main.Volume < MIN_VOL){ 
+           main.volume = MIN_VOL;
+       } 
+    }
+
+    public int getVol(){
+        return Volume; 
+    }
+}
