81e43a58d582bc65c1477a468ffedcf6bed2ee7a
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-01-26 02:16:39 -0500

    Final touches
    Whole lots of comments and smoothed over some working parts

--- Wk2/Project1.java
+++ Wk2/Project1.java
@@ -1,3 +1,11 @@
+/* 
+* File Name: Project1.java
+* Author: Tyler D Clark
+* Date: 25 Jan 2020
+* Purpose: Reads weights from a text file using JFileChooser and puts them into an array
+* of Weight objects. Uses methods to find the smallest, largest, and average weight and
+* prints them out.
+*/
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -6,47 +14,51 @@ import java.util.Scanner;
 import javax.swing.JFileChooser;
 
 class Project1{
-    
-    
-    
+    // Runs here
     public static void main(String[] args) throws IOException {
         
-        //Initialization of a bunch of stuff I need
+        /* 
+        Initialization of a bunch of stuff I need. I am only using an
+        ArrayList temporarily because of the .add method and no fixed size
+        */
         List<Weight> userWeights = new ArrayList<Weight>();
         File file;
-        Scanner filein;
+        Scanner fileIn;
         int response;
         JFileChooser chooser = new JFileChooser(".");
         
-        //only files and directories
+        // only files and directories
         chooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
         response = chooser.showOpenDialog(null);
 
-        /* a ton of conditionals, it hurts my eyes. If I did not have to follow a 
-        ** rubric, I would put the JFileChooser in a method... getFile() maybe?
+        /* 
+        a ton of conditionals, it hurts my eyes. If I did not have to follow a 
+        rubric, I would put the JFileChooser in a method... getFile() maybe?
         */
         if(response == JFileChooser.APPROVE_OPTION) {
             file = chooser.getSelectedFile();
-            //Inside try catch for errors
+            // Inside try catch for errors
             try {
-                filein = new Scanner(file);
+                fileIn = new Scanner(file);
                 if(file.isFile()){
                     int lines = 0;
-                    while(filein.hasNextLine()){
-                    
+                    while(fileIn.hasNextLine()){
                         lines++;
-                        //making a weight object for every value and then adding to arraylist
-                        Weight w = new Weight(filein.nextDouble());
-                        userWeights.add(w);
+                        // making a weight object for every value and then adding to arraylist
+                        int tempLb = fileIn.nextInt();
+                        double temptOz = fileIn.nextDouble();
+                        Weight tempW = new Weight(tempLb,temptOz);
+                        userWeights.add(tempW);
                         // maximum of 25 weights
                         if (lines > 25) {
-                            break;
+                            System.err.println("This program only accepts 25 or less weights");
+                            System.exit(0);
                         } //end if
                     } //end while
                 } else {
                     System.out.println("That's not a file.");
                 } // need to close scanner for resource leaks
-                filein.close();
+                fileIn.close();
             //just prints the error if we get one    
             } catch (Exception e) {
                 e.printStackTrace();
@@ -55,16 +67,17 @@ class Project1{
         //putting the weights into an array from the array list to meet rubric specifications
         Weight[] weightArr = userWeights.toArray(new Weight[0]);
 
-        /* printing the results from the class methods Weight class
-        ** the Weight class toString method overrides the Object class toString
+        /* 
+        printing the results from the class methods Weight class
+        the Weight class toString method overrides the Object class toString
         */
         System.out.println("Minimum weight: "+findMinimum(weightArr));
         System.out.println("Maximum weight: "+findMaximum(weightArr));
         System.out.println("Average weight: "+findAverage(weightArr));
     }
-    /* takes in the array of weight objects and loops through
-    ** using the Weight class lessThan() to determine the smallest
-    ** makes sense why we made it...
+    /* 
+    takes in the array of weight objects and loops through using the
+    Weight class lessThan() to determine the smallest. Makes sense why we made it...
     */
     private static Weight findMinimum(Weight[] w){
         Weight minWeight = w[0];
@@ -87,10 +100,11 @@ class Project1{
     }
     // using the other convenient Weight class methods to determine avg   
     private static Weight findAverage(Weight[] w){
-        Weight totalWeight = w[0];
-        for (int i = 1; i < w.length; i++) {
-            w[i].addTo(totalWeight);
+        Weight totalWeight = new Weight(0, 0);
+        for (int i = 0; i < w.length; i++) {
+            totalWeight.addTo(w[i]);;
         }
+        totalWeight.divide(w.length);
         return totalWeight;
     } 
 }
