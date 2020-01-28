42499ae8c72080270ea7b48d2af3afbbbdee22aa
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-01-27 22:35:08 -0500

    Overly Complicated will fix tomorrow

---
+++ Wk3/ExceptionExample.java
@@ -0,0 +1,21 @@
+public class ExceptionExample {
+    public static void main(String[] args) {
+
+    }
+    public File getFile() {
+        try{
+            choser = new JFileChooser();
+            chooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+            response = chooser.showOpenDialog(null);
+            if (response == JFileChooser.APPROVE_OPTION) {
+                file = chooser.getSelectedFile();
+                file.getIntArray();
+            }
+        } catch (notIntException) {
+            System.out.println("Needs to be ints");
+        }
+    }
+    public int[] getIntArray(File file) throws notIntException {
+        
+    }
+}
