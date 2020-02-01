0b44b615435472027527027936223fa1e56692ba
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-02-01 12:25:40 -0500

    Delete unneeded and put up final

---
+++ Wk3/ExampleException.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+
+public class ExampleException {
+    // Even main needs a throws
+    public static void main(String[] args) throws IOException {
+
+        ExampleException e = new ExampleException();
+
+        // first method is called without a try catch
+        e.first();
+    }
+
+    public ExampleException() {
+    }
+
+    // which calls the second
+    public void first() throws IOException{
+        second();
+    }
+    // which calls the third
+    public void second() throws IOException{
+        third();
+    }
+    // which throws a new IOExceptions
+    // Thats a whole lotta "throws"
+    public void third() throws IOException{
+        throw new IOException(); //the exception is now propagated back
+    }
+}
