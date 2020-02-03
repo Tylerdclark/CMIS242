51cb53bbb7e5dd320a2a1cc8f9f0a8f7f8600cd4
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-02-03 16:01:46 -0500

    First of week4 project. Starting the first class

---
+++ Wk4/Automobile.java
@@ -0,0 +1,10 @@
+class Automobile {
+    String make;
+    String model;
+    Int purchasePrice;
+
+    public Automobile(String make, int purchasePrice) {
+        this.make = make;
+        this.purchasePrice = purchasePrice;
+    }
+}
