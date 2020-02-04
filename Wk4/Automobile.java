e15b188ea6ceca686c0d2fc22a79eb9957de305c
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-02-03 23:13:19 -0500

    Next method and Main for debugging

--- Wk4/Automobile.java
+++ Wk4/Automobile.java
@@ -1,10 +1,16 @@
 class Automobile {
     String make;
     String model;
-    Int purchasePrice;
+    int purchasePrice;
 
+    //plain jane constructor
     public Automobile(String make, int purchasePrice) {
         this.make = make;
         this.purchasePrice = purchasePrice;
+    }
+    //crreating a salesTax method using type casting
+    public int salesTax(){
+        int tax = (int) (this.purchasePrice * (5 / 100.0f));
+        return tax;
     }
 }
---
+++ Wk4/Main.java
@@ -0,0 +1,6 @@
+class Main {
+    public static void main(String[] args) {
+        Automobile a = new Automobile("Honda", 100);
+        System.out.print(a.salesTax());
+    }
+}
