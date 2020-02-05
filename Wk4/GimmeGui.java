1a86c12360464959ea46413570d80013811a9723
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-02-05 15:14:29 -0500

    Main components of the Jframe

--- Wk4/GimmeGui.java
+++ Wk4/GimmeGui.java
@@ -1,36 +1,21 @@
-import javax.swing.*;
-import java.awt.*;
+import javax.swing.JFrame;
 
+public class GimmeGui {
 
-//note that it extends JFrame
-public class GimmeGui extends JFrame {
+  private JFrame myFrame;
 
-    public GimmeGui () {
-      this ("one");
-    } // end no parameter constructor
-  
-    public GimmeGui (String t) {
-        // next four lines are imperative
-      setTitle (t);
-      setSize (200,200);
-      setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
-      setLocationRelativeTo (null);
-      //adding the buttons
-      //we can add anything that extends JComponent
-      //add lets you add in a new component
-
-      add (new JButton ("North " + t), BorderLayout.NORTH);
-      add (new JButton ("East " + t), BorderLayout.EAST);
-      add (new JButton ("West " + t), BorderLayout.WEST);
-     
-      validate (); // check that everything makes sense
-      pack (); // pack it within the visible screen
-      setVisible (true); // make it visable
-    } // end String constructor
-  
-    public static void main (String args []) {
-        GimmeGui ga = new GimmeGui ();
-        GimmeGui gb = new GimmeGui ("two");
-    } // end main
-  
-  } // end class GuiExample
+  //put it in a method for aethstetics
+  public GimmeGui(){
+    myFrame = new JFrame("This is my title");
+    myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    myFrame.setSize(300, 300);
+  }
+  //show method.. can run in background
+  public void show(){
+    myFrame.setVisible(true);
+  }
+  public static void main(String[] args) {
+  GimmeGui gimmeGui = new GimmeGui();
+  gimmeGui.show();
+  }
+}
