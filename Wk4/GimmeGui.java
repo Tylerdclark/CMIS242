1a571b6e83ad849f46063e3493707c0ce631f48d
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-02-05 17:00:15 -0500

    Random windows when button is pressed

--- Wk4/GimmeGui.java
+++ Wk4/GimmeGui.java
@@ -1,21 +1,47 @@
+import java.awt.event.ActionListener;
+import java.util.concurrent.ThreadLocalRandom;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
-public class GimmeGui {
+public class GimmeGui implements ActionListener {
 
   private JFrame myFrame;
+  
 
   //put it in a method for aethstetics
   public GimmeGui(){
-    myFrame = new JFrame("This is my title");
+    myFrame = new JFrame("Windowception");
     myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     myFrame.setSize(300, 300);
+
+    //make a label
+    JLabel label = new JLabel("Press the button to make another window");
+    label.setHorizontalAlignment(JLabel.CENTER);
+    label.setVerticalAlignment(JLabel.NORTH);
+    myFrame.add(label);
+
+    JButton newWindow = new JButton("New Window");
+    newWindow.addActionListener(this);
+    myFrame.add(newWindow);
   }
+
+  public void actionPerformed(java.awt.event.ActionEvent e) {
+    GimmeGui gimmeGui = new GimmeGui();
+    gimmeGui.show();
+  }
+
   //show method.. can run in background
   public void show(){
+    int randIntx = ThreadLocalRandom.current().nextInt(1000);
+    int randInty = ThreadLocalRandom.current().nextInt(1000);
     myFrame.setVisible(true);
+    myFrame.setLocation(randIntx, randInty);
   }
   public static void main(String[] args) {
   GimmeGui gimmeGui = new GimmeGui();
   gimmeGui.show();
   }
+
+
 }
