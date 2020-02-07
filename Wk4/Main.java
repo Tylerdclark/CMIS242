5dad349d6db59f0ecd54bb246d0ed6b368f7c268
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-02-07 11:30:42 -0500

    Revert "Multiples on gitignore"
    This reverts commit 502a4fdfca4a34f574026db070f31ca0952c77c2.

--- Wk4/Main.java
+++ Wk4/Main.java
@@ -1,7 +1,6 @@
-
-
-public class Main {
-  public static void main(String[] args) {
-
-  }
+class Main {
+    public static void main(String[] args) {
+        Automobile a = new Automobile("Honda", 100);
+        System.out.print(a.salesTax());
+    }
 }
