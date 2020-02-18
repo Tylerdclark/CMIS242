a904343e12375d5f1919be160e83db6e9dfdbc5c
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-02-17 23:33:50 -0500

    Week 6

---
+++ IterativeExample.java
@@ -0,0 +1,13 @@
+public class IterativeExample {
+    public static void main(String[] args) {
+        
+    }
+
+    public static void sumNum(int n) {
+
+        int divisor = n/2;
+        while (divisor != 0) {
+
+        }
+    }
+}
---
+++ Wk6/Discussion/RecursionExample.java
@@ -0,0 +1,23 @@
+/**
+* File Name: RecursionExample.java
+* Author: Tyler D Clark
+* Date: 17 Feb 2020
+* Purpose: Demonstrates recursion by giving the sum of a number and the numbers less than it
+ */
+public class RecursionExample {
+    public static void main(String[] args) {
+        System.out.println(sumNum(10));
+    }
+
+    public static int sumNum(int n) {
+        if ( n < 0) {
+            System.out.print("This number will cause infinite recursion!: ");
+            return n;
+        // this if statement breaks out of the recursion    
+        }if (n == 1) {
+            return n;
+        }else {
+            return (n + sumNum(n - 1));
+        }
+    }
+}    
