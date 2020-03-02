a6e8ea126b6443dff6e077758f492d94aaaf8cd4
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-03-02 18:25:29 -0500

    first stage of GUI

--- wk8/project4/GUI.java
+++ wk8/project4/GUI.java
@@ -1,16 +1,22 @@
 package wk8.project4;
 
 
+
 import java.awt.GridLayout;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 
-public class GUI extends JFrame {
+public class GUI<Status> extends JFrame {
 
 	private static final long serialVersionUID = 1L;
+	
+	
 	
 	/*	JLabels	*/
 	JLabel transactionJLabel = new JLabel("Transaction No:");
@@ -26,7 +32,15 @@ public class GUI extends JFrame {
 	JTextField squareFootageField = new JTextField();
 	JTextField priceField = new JTextField();
 	
-	/*	JButtons*/
+	/*	JButtons */
+	JButton processBtn = new JButton("Process");
+	JButton changeStatusBtn = new JButton("Change Status");
+	
+	/*	JComboboxes	*/
+	String[] processeStrings = {"Insert", "Delete", "Find"};
+	JComboBox<String> processComboBox = new JComboBox<>(processeStrings);
+	JComboBox<Status> statusComboBox = new JComboBox<>();
+	
 	
 	
 
@@ -44,16 +58,34 @@ public class GUI extends JFrame {
 	// Create the frame
 	public GUI() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 425, 450);
+		setBounds(100, 100, 325, 275);
 		setResizable(false);
 		setContentPane(mainPanel());
+		
 	}
 	// Content panel
+	// TODO: split into left and right panels with flow layouts
 	public JPanel mainPanel() {
-		JPanel mainPanel = new JPanel();
-		mainPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
-		mainPanel.setLayout(new GridLayout(7, 2));
-		return mainPanel;
+		JPanel thisPanel = new JPanel();
+		thisPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		thisPanel.setLayout(new GridLayout(7, 2));
+		thisPanel.add(transactionJLabel);
+		thisPanel.add(transactionField);
+		thisPanel.add(addressJLabel);
+		thisPanel.add(addressField);
+		thisPanel.add(bedroomsJLabel);
+		thisPanel.add(bedroomsField);
+		thisPanel.add(squareFootageJLabel);
+		thisPanel.add(squareFootageField);
+		thisPanel.add(priceJLabel);
+		thisPanel.add(priceField);
+		thisPanel.add(processBtn);
+		thisPanel.add(processComboBox);
+		thisPanel.add(changeStatusBtn);
+		thisPanel.add(statusComboBox);
+		
+		
+		return thisPanel;
 		
 
 		
