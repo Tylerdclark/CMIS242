ac88e28ea52d4dcf79e5fb8f659c6dc4abd3efbc
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-03-03 11:40:19 -0500

    First button action

--- wk8/project4/Project4.java
+++ wk8/project4/Project4.java
@@ -1,6 +1,10 @@
 package wk8.project4;
 
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.TreeMap;
+
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
@@ -9,9 +13,11 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 
-public class Project4 extends JFrame {
+
+public class Project4 extends JFrame implements ActionListener {
 
 	private static final long serialVersionUID = 1L;
+	private TreeMap<Integer, Property> properTreeMap = new TreeMap<>();
 	
 	/*	JLabels	*/
 	JLabel transactionJLabel = new JLabel("Transaction No:");
@@ -37,13 +43,14 @@ public class Project4 extends JFrame {
 	JComboBox<Status> statusComboBox = new JComboBox<>();
 
 	public static void main(String[] args) {
+		
+		Property property = new Property("", 1, 1, 1);
+		System.out.println(property);
+		property.changeState(Status.UNDER_CONTRACT);
+		System.out.println(property);
+		Project4 frame = new Project4();
+		frame.setVisible(true);
 
-				try {
-					Project4 frame = new Project4();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
 	
 	}
 	// Create the frame
@@ -58,7 +65,6 @@ public class Project4 extends JFrame {
 	}
 	
 	// Content panel
-
 	public JPanel mainPanel() {
 		JPanel thisPanel = new JPanel();
 		thisPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -67,6 +73,7 @@ public class Project4 extends JFrame {
 		thisPanel.add(rightPanel());
 		return thisPanel;
 	}
+	// split into left
 	public JPanel leftPanel() {
 		JPanel thisPanel = new JPanel();
 		thisPanel.setLayout(new GridLayout(7, 1, 0, 0));
@@ -76,10 +83,11 @@ public class Project4 extends JFrame {
 		thisPanel.add(squareFootageJLabel);
 		thisPanel.add(priceJLabel);
 		thisPanel.add(processBtn);
+		processBtn.addActionListener(this);
 		thisPanel.add(changeStatusBtn);
 		return thisPanel;
 	}
-	
+	// and right panel
 	public JPanel rightPanel() {
 		JPanel thisPanel = new JPanel();
 		thisPanel.setLayout(new GridLayout(7, 1, 0, 0));
@@ -92,4 +100,34 @@ public class Project4 extends JFrame {
 		thisPanel.add(statusComboBox);
 		return thisPanel;
 	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		try {
+			if (e.getSource() == processBtn) {
+				if (processComboBox.getSelectedItem() == "Insert") {
+					insertProperty();
+				} else if(processComboBox.getSelectedItem() == "Delete") {
+					
+				} else if (processComboBox.getSelectedItem() == "Find") {
+					
+				}
+			}
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		}
+		
+	}
+	
+	private void insertProperty() {
+		int trans = Integer.parseInt(transactionField.getText());
+		Property insertedProperty = new Property(
+				addressField.getText(), 
+				Integer.parseInt(bedroomsField.getText()), 
+				Integer.parseInt(squareFootageField.getText()), 
+				Integer.parseInt(priceField.getText()));
+		properTreeMap.put(trans, insertedProperty);
+		
+	}
+	
 }
